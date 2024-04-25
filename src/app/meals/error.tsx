d00ee934ae05cd4b5639cc12bf16bf8622591397
Bartek F <bartekfr@wp.pl>
2024-04-25 10:16:41 +0200

    Add meals error page

---
+++ src/app/meals/error.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+export default function Error() {
+  return (
+    <>
+      <main className='error'>
+        <h1>Error occured</h1>
+      </main>
+    </>
+  )
+}
--- src/lib/meals.ts
+++ src/lib/meals.ts
@@ -5,5 +5,6 @@ const db = sql('meals.db')
 
 export async function getMeals(): Promise<Meal[]> {
   await new Promise( resolve => setTimeout(resolve, 4000))
+  // throw new Error('Fail')
   return db.prepare('SELECT * FROM MEALS').all() as Meal[];
 }
