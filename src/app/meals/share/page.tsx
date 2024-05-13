3cc83dfb4a229cd7d1a79946b553d04a0e014e7b
Bartek F <bartekfr@wp.pl>
2024-05-13 12:38:22 +0200

    Show form submission errors

--- src/app/meals/share/page.tsx
+++ src/app/meals/share/page.tsx
@@ -1,9 +1,14 @@
+'use client';
+
 import classes from './page.module.css';
 import ImagePicker from '@/components/meals/image-picker'
 import MealsFormSubmit from '@/components/meals/meals-form-submit';
 import { shareMeal } from '@/lib/actions';
+import { useFormState } from 'react-dom';
 
 export default function ShareMealPage() {
+  // hook renamed to useActionState in React canary
+  const [state, formAction] = useFormState(shareMeal, { message: '' });
   return (
     <>
       <header className={classes.header}>
@@ -13,7 +18,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -21,7 +26,7 @@ export default function ShareMealPage() {
             </p>
             <p>
               <label htmlFor="email">Your email</label>
-              <input type="email" id="email" name="email" required />
+              <input type="text" id="email" name="email" required />
             </p>
           </div>
           <p>
@@ -42,6 +47,9 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker name='image' label='image'/>
+          <p>
+            {state.message}
+          </p>
           <p className={classes.actions}>
             <MealsFormSubmit />
           </p>
--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -5,7 +5,7 @@ import { MealInput } from '@/types'
 import { saveMeal } from './meals';
 import { redirect } from 'next/navigation';
 
-export async function shareMeal(formData: FormData) {
+export async function shareMeal(prevState: any, formData: FormData) {
   const meal: MealInput = {
     title: formData.get('title') as string,
     creator: formData.get('name') as string,
@@ -14,8 +14,20 @@ export async function shareMeal(formData: FormData) {
     instructions: formData.get('instructions') as string,
     image: formData.get('image') as File,
   }
+  // TODO: validate meal data
+  if (!meal.creator_email.includes('@')) {
+    // throw new Error('Invalid email');
+
+    return {
+      message: 'Invalid email',
+    }
+  }
 
   await saveMeal(meal)
   redirect('/meals')
+
+  return {
+    message: 'Form submitted',
+  }
 
 }
